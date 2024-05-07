e9bd5b95e7dbc136c64623a1cbb0ba5b542c4e8f
NuReach <nureach46@gmail.com>
2024-05-07 15:54:15 +0700

    dashboard update

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Footer from '../../Components/Frontend-Footer/Footer'
 import NavbarDashboard from '../../Components/Dashboard-Navbar/NavbarDashboard'
 import CardDashbaord from '@/Components/Card/CardDashbaord';
-import { AreaChart, Book, Code, Flag, HandCoins, Languages, Newspaper, User } from 'lucide-react';
+import { AreaChart, Book, Code, Flag, HandCoins, Languages, Newspaper, User, UserCheck, Video } from 'lucide-react';
 import CountryForm from '@/Components/Form/CountryForm';
 import LanguageForm from '@/Components/Form/LanguageForm';
 import ProgramForm from '@/Components/Form/ProgramForm';
@@ -17,15 +17,17 @@ export default function Dashboard() {
             <div>
                 <p className='font-bold text-3xl'>Dashboard</p>
             </div>
-            <div className=' columns-1 sm:columns-2 lg:columns-3 xl:columns-4 space-y-3  gap-3'>
+            <div className=' columns-1 break-after-column sm:columns-2 lg:columns-3 xl:columns-3 space-y-6 gap-6 '>
                 <CardDashbaord title={"Posts"} value={"+150"} icon={<Newspaper />} />
                 <CardDashbaord title={"Users"} value={"+10"} icon={<User/>} />
                 <CardDashbaord title={"Courses"} value={"+10"} icon={<Book />} />
+                <CardDashbaord title={"Videos"} value={"+10"} icon={<Video/>} />
                 <CardDashbaord title={"Countries"} value={"+10"} icon={<Flag />} />
                 <CardDashbaord title={"Languages"} value={"+3"} icon={<Languages />} />
                 <CardDashbaord title={"Programs"} value={"+10"} icon={<Code />} />
                 <CardDashbaord title={"Statuses"} value={"+10"} icon={<AreaChart />} />
                 <CardDashbaord title={"Service"} value={"+4"} icon={<HandCoins />} />
+                <CardDashbaord title={"Admin"} value={"+1"} icon={<UserCheck />} />
             </div>
             <div>
                 <p className='font-bold text-3xl'>Update Content</p>
