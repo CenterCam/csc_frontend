e57d5d7082ec1ea7f53fd30e1f6d5fd0862169d3
NuReach <nureach46@gmail.com>
2024-05-22 02:04:19 +0700

    course page

--- src/Components/Card/CourseCard.jsx
+++ src/Components/Card/CourseCard.jsx
@@ -1,29 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from '../ui/button'
 
-export default function CourseCard() {
+export default function CourseCard({item}) {
   return (
-    <div class="course flex flex-col p-1 border m-3 justify-start items-start w-80 ">
+    <div className="course flex flex-col p-1 border justify-start items-start w-full h-[420px] overflow-hidden ">
         <div className="left w-full">
             <img
-              src="https://play-lh.googleusercontent.com/DAjoLy_iFvFDWypZiQa8WALBpARu-0nui8VjiWzo5rm85A15pF9iHwNFIXC7pFqgbaE"
+              src={item.image}
               alt=""
               className=" h-56 w-full object-cover"
             />
         </div>
         <div className='my-1 p-3'>
-            <a href="" className='font-bold text-lg line-clamp-2 cursor-pointer hover:underline '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptates voluptas eum dolorum autem dignissimos cupiditate eligendi earum ut veniam, unde consequatur, quod obcaecati. Magni ducimus consectetur dicta laboriosam ipsam?
-            </a>
-            <p className="my-1 line-clamp-2">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ratione inventore facilis, perferendis rerum consequatur explicabo tempore temporibus? Error nulla nihil ea fugiat nisi vero eaque fuga, est in repellat.
+            <Link to="" className='font-bold text-lg line-clamp-2 cursor-pointer hover:underline capitalize'>
+              {item.title}
+            </Link>
+            <p className="my-1 line-clamp-2 capitalize">
+              {item.desc}
             </p>
             <div className=" flex space-x-3">
               <p className="text-gray-500 text-sm">Created By : </p>
-              <span className="text-gray-500 text-sm">Reach</span>
+              <span className="text-gray-500 text-sm capitalize">{item.user.name}</span>
             </div>
             <div className="flex space-x-3">
-              <p className="text-gray-500 text-sm ">Duration : 12 h Total</p>
+              <p className="text-gray-500 text-sm ">Duration : {item.duration}</p>
               <p className="text-orange-500 text-sm font-bold">2025-15-02</p>
             </div>
+            {
+              item.type == "paid" ?
+              <Button className="bg-orange-400 my-1 hover:bg-orange-600">${(item.price)}</Button> :
+              <Button className="bg-green-400 hover:bg-green-600">Free</Button>
+            }
         </div>
     </div>
   )
--- src/Pages/Course.jsx
+++ src/Pages/Course.jsx
@@ -1,36 +1,64 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Navbar from '../Components/Frontend-Navbar/Navbar'
 import Footer from '../Components/Frontend-Footer/Footer'
 import CourseCard from '../Components/Card/CourseCard'
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { Store } from '@/Utils/Store';
+import axios from 'axios';
+import { proxy } from '@/Utils/Utils';
+import { Input } from '@/Components/ui/input';
+import MyPagination from '@/Components/Pagination/MyPagination';
 
 export default function Course() {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const search = queryParams.get("search") || "all";
+  const sortBy = queryParams.get("sortBy") || "created_at";
+  const sortDir = queryParams.get("sortDir") || "desc";
+  const page =  queryParams.get("page") || 1;
+  const {state , dispatch} = useContext(Store);
+  const {csc_user} = state;
+  const navigate = useNavigate();
+  const {isLoading , isError, data:courses} = useQuery({ 
+      queryKey: ['courses',{search,sortBy,sortDir}], 
+      queryFn: async ()=>{
+          try {
+              const response = await axios.get(`${proxy}/api/course/${search}/${sortBy}/${sortDir}?page=${page}`);
+              return response.data;
+          } catch (error) {
+              throw error;
+          }
+      }
+    });
+    console.log(courses);
   return (
     <div>
         <Navbar page={"/course"} />
-        <section className='flex justify-center items-center my-3'>
-          <div className=" relative flex items-center">
-              <input name="keyword" type="text" className="border-none rounded-lg p-3 border-gray-700 w-96 focus:border-none focus:scale-105 transition-all focus:ring-0 shadow-lg" placeholder="Search course..." />
-              <svg className="absolute right-2 top-2 " xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M9.538 15.23q-2.398 0-4.064-1.666T3.808 9.5q0-2.398 1.666-4.064t4.064-1.667q2.399 0 4.065 1.667q1.666 1.666 1.666 4.064q0 1.042-.369 2.017q-.37.975-.97 1.668l5.908 5.907q.14.14.15.345q.01.203-.15.363q-.16.16-.353.16q-.195 0-.354-.16l-5.908-5.908q-.75.639-1.725.989q-.975.35-1.96.35Zm0-1q1.99 0 3.361-1.37q1.37-1.37 1.37-3.36q0-1.99-1.37-3.36q-1.37-1.37-3.36-1.37q-1.99 0-3.361 1.37q-1.37 1.37-1.37 3.36q0 1.99 1.37 3.36q1.37 1.37 3.36 1.37Z"/></svg>
-          </div>
-        </section>
+
         <section className="px-9 md:px-24 mb-9">
-          <div class="font-bold text-lg border-b">Free Course</div>
-          <div className='flex justify-center flex-wrap'>
-            <CourseCard />
-            <CourseCard />
-            <CourseCard />
+          <div className='flex items-center gap-3 my-6 border-b pb-6'>
+            <div className="font-bold text-lg ">Course</div>
+            <div>
+              <Input className="w-full sm:w-96" onChange={(e)=>navigate(`/course?search=${e.target.value}&&sortBy=${sortBy}&&sortDir=${sortDir}&&page=${page}`)} type="text"  placeholder="Search..." />
+            </div>
           </div>
-          <div className=" flex justify-end text-orange-600 font-bold text-xs mt-3 "><a href="search/seemore/bachelor">SEE MORE</a></div>         
-        </section>
-        <section className="px-9 md:px-24 mb-9">
-          <div class="font-bold text-lg border-b">Paid Course</div>
-          <div className='flex justify-center flex-wrap'>
-            <CourseCard />
-            <CourseCard />
-            <CourseCard />
+          <div className='columns-1 md:columns-2 xl:columns-3 space-y-9  px-6 md:px-14 lg:px-36 w-full'>
+            {
+              courses?.data.map((item,i)=>(
+                <CourseCard key={i} item={item} />  
+              ))
+            }
           </div>
-          <div className=" flex justify-end text-orange-600 font-bold text-xs mt-3 "><a href="search/seemore/bachelor">SEE MORE</a></div>   
         </section>
+        <div className='w-full flex justify-end mt-3 px-9'>
+          <MyPagination
+              url = {`/course?search=${search}&&sortBy=${sortBy}&&sortDir=${sortDir}`}
+              links = {courses?.links}
+              total = {courses?.total}
+              current = {courses?.to}
+          />
+        </div>
         <Footer />
     </div>
   )
