a1316bd95be014441852c8a163eabc57efca2f9b
NuReach <nureach46@gmail.com>
2024-05-28 00:23:50 +0700

    chnage proxy

--- src/Utils/Utils.jsx
+++ src/Utils/Utils.jsx
@@ -1,5 +1,6 @@
-// export const proxy = "https://hongnnureach.info/";
-export const proxy = "http://127.0.0.1:8000";
+export const proxy = "https://hongnnureach.info/";
+// export const proxy = "http://127.0.0.1:8000";
+
 
 
 export const convertDateToLaravelFormat = (dateString) => {
